eb17e5125cd9aa50661d09e875cfbd5dd900ffa5
Damian Pokorski <damianpokorski@hotmail.com>
2018-07-08 18:06:57 +0100

    Added Babel loader, static struct copying, scripts in packages.json

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,13 +1,30 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+// Paths
+const dist = path.resolve(__dirname, '../dist');
+const src = path.resolve(__dirname, '../src');
+const static = path.resolve(__dirname, '../src/static');
+
+// Config
 module.exports = {
     entry: './src/entry.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, '../dist')
+        path: dist
     },
     module: {
         rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }, {
             test: /\.css$/,
             use: [
                 'style-loader',
@@ -29,5 +46,11 @@ module.exports = {
                 }
             },
         }]
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(dist),
+        new CopyWebpackPlugin([
+            { from: static, to: dist }
+        ])
+    ]
 };
